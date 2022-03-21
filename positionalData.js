8d49a16f52738c4ca4e3fcaa3504ea3d3e897a76
Darrel Belvin <darrel.belvin@gmail.com>
2022-03-20 21:10:10 -0700

    semicolons, .env, and no undefined data

--- positionalData.js
+++ positionalData.js
@@ -1,11 +1,13 @@
 const { v4: uuidv4 } = require('uuid');
-const bedrock = require('bedrock-protocol')
+const bedrock = require('bedrock-protocol');
+require('dotenv').config();
+
 const client = bedrock.createClient({
   host: 'localhost',   // server address
   port: 19132,         // server port
-  username: '',   // the username you want to use as observer, optional if online mode
+  username: process.env.USER_ID,   // the username you want to use as observer, optional if online mode
   offline: false       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
-})
+});
 
 const detectionInterval = 500; // refresh frequency in miliseconds
 const facingLength = 50;  // length of vector for detecting where the player is looking
@@ -14,41 +16,49 @@ var locationCommandUUID;
 var facingCommandUUID;
 var locations;
 var facings;
+var lastPositionalData;
 
 client.on('spawn', () => {
-  console.log('Spawned in')
+  console.log('Spawned in');
   setInterval(sendDetectCommand, detectionInterval);
-})
+});
 
 client.on('command_output', (packet) => { // Listen for replies to commands
   if(packet.origin.uuid === locationCommandUUID){
-    locations = Object()
-    const keys = packet.output.pop(packet.output.length).parameters[1].split(',').map((s) => s.trim())
+    locations = Object();
+    const keys = packet.output.pop(packet.output.length).parameters[1].split(',').map((s) => s.trim());
     packet.output.forEach((x, i) => {
-      locations[keys[i]] = x.parameters.slice(0,3).map((x) => parseInt(x))
-    })
+      locations[keys[i]] = x.parameters.slice(0,3).map((x) => parseInt(x));
+    });
+    locationCommandUUID = undefined;
   }
   if(packet.origin.uuid === facingCommandUUID){
-    facings = Object()
-    const keys = packet.output.pop(packet.output.length).parameters[1].split(',').map((s) => s.trim())
+    facings = Object();
+    const keys = packet.output.pop(packet.output.length).parameters[1].split(',').map((s) => s.trim());
     packet.output.forEach((x, i) => {
-      facings[keys[i]] = x.parameters.slice(0,3).map((x) => parseInt(x))
-    })
+      facings[keys[i]] = x.parameters.slice(0,3).map((x) => parseInt(x));
+    });
+    facingCommandUUID = undefined;
   }
-  if((locations != undefined) && (facings != undefined)) {
-    var output = Object()
+  if((locationCommandUUID === undefined) && (facingCommandUUID === undefined)) {
+    var output = Object();
     Object.keys(locations).forEach((k) => {
       output[k] = {
         location: locations[k],
         facing: facings[k].map((x,i)=>(x-locations[k][i])/facingLength)
+      };
+      if((output[k].facing.length == 0) && (k in lastPositionalData)){
+        output[k].facing = lastPositionalData[k].facing;
       }
-    })
-    console.log(output)
+    });
+    lastPositionalData = output;
+    lastPositionalData['timestamp'] = Date.now()
+    //console.log(lastPositionalData)
   }
-})
+});
 
 function sendCommand(commandStr){
-  const uuid = uuidv4()
+  const uuid = uuidv4();
   client.queue('command_request', {
     command: commandStr,
     origin: {
@@ -58,13 +68,11 @@ function sendCommand(commandStr){
       player_entity_id: undefined
     },
     interval: false
-  })
+  });
   return uuid
 }
 
 function sendDetectCommand(){
-  locations = undefined
-  facings = undefined
-  locationCommandUUID = sendCommand('/execute @a ~ ~ ~ testforblock ~ ~ ~ structure_void')
-  facingCommandUUID = sendCommand('/execute @a ~ ~ ~ testforblock ^ ^ ^' + facingLength + ' structure_void')
+  locationCommandUUID = sendCommand('/execute @a ~ ~ ~ testforblock ~ ~ ~ structure_void');
+  facingCommandUUID = sendCommand('/execute @a ~ ~ ~ testforblock ^ ^ ^' + facingLength + ' structure_void');
 }
